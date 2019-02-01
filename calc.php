ceb602da6cba9ad8ac97a7435e232ecbbae771fe
draktas <47016325+draktas@users.noreply.github.com>
2019-02-01 16:30:20 +0500

    Update calc.php

--- calc.php
+++ calc.php
@@ -6,27 +6,31 @@ $arr=explode(".",$datej);
 $pop=$_POST['pop'];
 $pops=$_POST['pops'];
 $summa="";
-
+//$difference = intval(abs( strtotime($datej) - strtotime('1.2.2020') ));
+//(date("t", mktime(0, 0, 0, $arr [1], $arr [0], $arr [2])) - $arr [0])
 if ($pop == 'Да'){
 	if ($arr [2] == 2020 || $arr [2] == 2024 || $arr [2] == 2028 || $arr [2] == 2032){
-		$summa = $sum + ($sum + $pops)*(date("t", mktime(0, 0, 0, $arr [1], $arr [0], $arr [2])) - $arr [0] + 1)*(0.1 / 366);
-		echo "Результат: $summa";
+		
+		$summa = round ($sum + ($sum + $pops)*($god*366)*(0.1 / 366));
+		echo "Результат: $summa руб.";
 		
 	}
 	else {
-		$summa = $sum + ($sum + $pops)*(date("t", mktime(0, 0, 0, $arr [1], $arr [0], $arr [2])) - $arr [0] + 1)*(0.1 / 365);
-		echo "Результат: $summa";
+		$summa = round ($sum + ($sum + $pops)*($god*365)*(0.1 / 365));
+		echo "Результат: $summa руб.";
 		};
 }
 else {
 	if ($arr [2] == 2020 || $arr [2] == 2024 || $arr [2] == 2028 || $arr [2] == 2032){
-		$summa = $sum + ($sum)*(date("t", mktime(0, 0, 0, $arr [1], $arr [0], $arr [2])) - $arr [0] + 1)*(0.1 / 366);
-		echo "Результат: $summa";
+		$summa = round ($sum + ($sum)*($god*366)*(0.1 / 366));
+		echo "Результат: $summa руб.";
+	
 		
 	}
 	else {
-		$summa = $sum + ($sum)*(date("t", mktime(0, 0, 0, $arr [1], $arr [0], $arr [2])) - $arr [0] + 1)*(0.1 / 365);
-		echo "Результат: $summa";
+		$summa = round ($sum + ($sum)*($god*365)*(0.1 / 365));
+		echo "Результат: $summa руб.";
+	
 		};
 };
 ?>
